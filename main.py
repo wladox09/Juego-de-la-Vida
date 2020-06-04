c7c3f2069aefc981184d9d38d60e8cfde80f7ca1
wladimir <wladimir09@gmail.com>
2020-06-04 16:27:36 -0400

    probando pygame

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import numpy as numpy
+import time
+
+WIDTH, HEIGHT = 800, 800
+
+# Initialize pygame
+pygame.init()
+
+# Set size of screen
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
